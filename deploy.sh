0d210f67ee55ad85c4dd2b5133042a4a7a32346a
Mikhail Chernyshev <newmishach@gmail.com>
2023-04-25 18:16:37 +0300

    ghpagesdone

--- deploy.sh
+++ deploy.sh
@@ -12,15 +12,14 @@ cd dist
 
 # если вы публикуете на пользовательский домен
 # echo 'www.example.com' > CNAME
-
-# git init
-# git add -A
-# git commit -m 'deploy'
+git init
+git add -A
+git commit -m 'deploy'
 
 # если вы публикуете по адресу https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Mikhail-Chernyshev/test-for-banki-shop.git master:gh-pages
+# git push -f git@github.com:Mikhail-Chernyshev/test-for-banki-shop.git master:gh-pages
 
-# cd -
+ cd -
